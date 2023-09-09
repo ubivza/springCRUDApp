5e433d5cb774c5b0746f6b03f2975b5d39d92c4f
Aleksandr Belov <uka_ka@inbox.ru>
2023-09-09 20:38:43 +0300

    Initial commit

---
+++ src/main/java/spring/app/crud/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package spring.app.crud.dao;
+
+import org.springframework.stereotype.Component;
+import spring.app.crud.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int peopleCount;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++peopleCount, "Ivan"));
+        people.add(new Person(++peopleCount, "Aleksey"));
+        people.add(new Person(++peopleCount, "Ksenya"));
+        people.add(new Person(++peopleCount, "Varvara"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        people.add(new Person(++peopleCount, person.getName()));
+    }
+}
